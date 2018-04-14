93d2f144184b00e2c7437744db4c2509438263c0
chai2010 <chaishushan@gmail.com>
2018-04-14 10:38:06 +0800

    增加一个最简cgo例子

---
+++ demos/01-hello-cgo/hello-cgo-v1.go
@@ -0,0 +1,13 @@
+// Copyright 2018 <chaishushan{AT}gmail.com>. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// +build ignore
+
+package main
+
+import "C"
+
+func main() {
+	println("hello cgo")
+}
